e630c6a7e3bcf0d251e10eb5c6ec63a4a01d2dcb
Alex Mitcu <alexandru.mitcu@student.manchester.ac.uk>
2016-03-07 16:43:28 +0000

    Change

--- receive-sms.php
+++ receive-sms.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-require "/home/alex/Twilio/twilio-php-master/Services/Twilio.php"; // Loads the library
+require "/home/alex/Twilio/twilio-php-master/Services/Twilio.php";
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "YOUR SID";
-$token = "YOUR AUTH TOKEN";
+$sid = "AC377f38194b7c2ef80593aedae8be033f";
+$token = "49583d53d8a1eb4fdbfcf8aa9f44117c";
 $client = new \Services_Twilio($sid, $token);
 
 // Loop over the list of messages and echo a property for each one
